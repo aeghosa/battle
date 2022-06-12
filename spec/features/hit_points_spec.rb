faf953a753f995729ed616a7e9ae00b27ca405fb
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-06-13 00:04:53 +0100

    Viewing hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,5 @@
-feature 'Enter names' do
-  scenario 'submitting names' do
+feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
     visit('/')
     fill_in :player_1_name, with: 'Charlotte'
     fill_in :player_2_name, with: 'Mittens'
@@ -7,9 +7,11 @@ feature 'Enter names' do
 
     # save_and_open_page # will save the web page and open the browser to display it
 
-    expect(page).to have_content 'Charlotte vs. Mittens'
+    expect(page).to have_content 'Mittens: 60HP'
   end
 end
+
+
 
 # Visit the home page ('/')
 # Fill in a text field with a name
