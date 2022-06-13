efaebb0cd12319c7906e11a6eebb6d43abeb3c99
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-06-13 06:23:58 +0100

    Attacking Player 2

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,12 @@
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    save_and_open_page # will save the web page and open the browser to display it
+    expect(page).to have_content 'Charlotte attacked Mittens'
+  end
+end
+
+# As Player 1,
+# So I can win a game of Battle,
+# I want to attack Player 2, and I want to get a confirmation
